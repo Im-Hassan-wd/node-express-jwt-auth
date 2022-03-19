32aa4cc8f61793b00de22e0617449be78499dd01
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-19 18:00:32 +0100

    Auth Routes & Controllers

--- models/User.js
+++ models/User.js
@@ -1,1 +1,23 @@
 const mongoose = require('mongoose');
+
+const userScheme = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String, 
+    required: true, 
+    minlength: 6
+  },
+  display_name: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+});
+
+const User = mongoose.model('user')
