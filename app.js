6d62699c3bb8d733a885a8ec43322d91bad1c9ac
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-21 16:55:24 +0100

    Cookies primer

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
-app.use(cookie-parser());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -28,7 +28,10 @@ app.use(authRoutes);
 // cookies
 app.get('/set-cookies', (req, res) => {
 
-  res.setHeader('Set-Cookie', 'newUser=true');
+  //res.setHeader('Set-Cookie', 'newUser=true');
+
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true)
 
   res.send('you get the cookie');
 });
