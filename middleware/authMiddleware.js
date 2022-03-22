71f7418b9dd732212d94aa5c5c670c5337c2f8b0
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-22 17:28:45 +0100

    Logging User Out

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -17,6 +18,24 @@ const requireAuth = (req, res, next) => {
   }
   else {
    res.redirect('/login');
+  }
+}
+
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, 'weird warwick secret', (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id)
+        next();
+      }
+    });
   }
 }
 
