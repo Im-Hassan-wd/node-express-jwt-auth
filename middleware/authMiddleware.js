c99c50f3b034c46257e969a9c97507d507b59cee
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-22 14:14:34 +0100

    Protecting routes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 const requireAuth = (req, res, next) => {
+  const token = res.cookie.jwt;
 
+  // check json web token exits & is verified
+  if (token) {
+    jwt.verify(token, 'weird warwick secret')
+  }
+  else {
+   res.redirect('/login');
+  }
 }
