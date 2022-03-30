59c43e3a0f7a1d11f1eaa908e9e608c7ddb06811
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-30 03:43:46 +0100

    signup template and logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,10 +52,10 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.signup_post = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, display_name } = req.body;
 
   try {
-    const user = await User.create({ email, password });
+    const user = await User.create({ email, password, display_name });
     const token = createToken(user._id);
     res.cookie('jwt', token, { httpOnly: true, maxAge: expiry * 1000});
     res.status(201).json({ user: user._id});
